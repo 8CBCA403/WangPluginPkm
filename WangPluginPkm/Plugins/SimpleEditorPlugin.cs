f5813438cfb00d3a5faa2cd7a7774293caba596b
Sadlamulk <c18519667281@gmail.com>
2023-07-16 11:08:27 +0800

    Update SimpleEditorPlugin.cs

--- WangPluginPkm/Plugins/SimpleEditorPlugin.cs
+++ WangPluginPkm/Plugins/SimpleEditorPlugin.cs
@@ -12,11 +12,8 @@ using System.Drawing;
 using iText.Kernel.Font;
 using System.Globalization;
 using System.Net.Http;
-using Org.BouncyCastle.Utilities.Zlib;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using static System.Net.WebRequestMethods;
-
 
 namespace WangPluginPkm.Plugins
 {
@@ -25,17 +22,23 @@ namespace WangPluginPkm.Plugins
         public override string Name => "常用功能/Simple Editor";
         public override int Priority => 11;
         public static GameStrings GameStringsZh = GameInfo.GetStrings("zh");
-
         protected override void AddPluginControl(ToolStripDropDownItem modmenu)
         {
-            var ctrl = new ToolStripMenuItem(Name)
+          /*  var ctrl = new ToolStripMenuItem(Name)
             {
                 Image = Properties.Resources.pokeball
             };
             ctrl.Click += OpenForm;
             ctrl.Name = "常用功能/Simple Editor";
             modmenu.DropDownItems.Add(ctrl);
+          */
             AddCheckerToList();
+        }
+        private void OpenForm(object sender, EventArgs e)
+        {
+
+            var form = new SimpleEditor(SaveFileEditor, PKMEditor);
+            form.Show();
         }
         private void AddCheckerToList()
         {
@@ -43,83 +46,79 @@ namespace WangPluginPkm.Plugins
             menuVSD.Opening += (s, e) =>
             {
                 var info = GetSenderInfo(ref s!);
-               
-                if (info.Slot.Origin == SlotOrigin.Box && info.ReadCurrent().Species != (int)Species.None)
+                if (info.Slot.Origin == SlotOrigin.Box && info.ReadCurrent().Species != (int)Species.None && info.CanWriteTo())
                 {
                     ToolStripMenuItem insertSlotButton = new ToolStripMenuItem("在此处插空");
                     insertSlotButton.Image = Properties.Resources.Down;
                     menuVSD.Items.Add(insertSlotButton);
                     insertSlotButton.Click += (s, e) =>
-                           InsertSlot(SaveFileEditor.CurrentBox, info.Slot.Slot);
-                    menuVSD.Closing += (s, e) => menuVSD.Items.Remove(insertSlotButton);
-                 
-                        var pk = info.Slot.Read(SaveFileEditor.SAV);
-                        PKM p=PKMEditor.Data;
-                        var la = new LegalityAnalysis(pk);
-                        var ATKIVEV = new ToolStripMenuItem("物攻手");
-                        var SPAIVEV = new ToolStripMenuItem("特攻手");
-                        var ATK_0SPEIVEV = new ToolStripMenuItem("物攻0速");
-                        var SPA_0SPEIVEV = new ToolStripMenuItem("特攻0速");
-                        var TANKIVEV = new ToolStripMenuItem("坦克");
-                        var IVEVN = new ToolStripMenuItem("快捷三维编辑器");
-                        var SavePDF = new ToolStripMenuItem("打印检测报告PDF");
-                        var clearnick = new ToolStripMenuItem("清除昵称垃圾字节");
-          
-                        var changeid = new ToolStripMenuItem("把id由PKHeX改成存档id(仅适用于SV)");
-                        IVEVN.Image = Properties.Resources.Atom;
-                        SavePDF.Image = Properties.Resources.Report;
-                        clearnick.Image = Properties.Resources.TrashCan;
+                         InsertSlot(SaveFileEditor.CurrentBox, info.Slot.Slot);
+                    var pk = info.Slot.Read(SaveFileEditor.SAV);
+                    PKM p=PKMEditor.Data;
+                    var la = new LegalityAnalysis(pk);
+                    var ATKIVEV = new ToolStripMenuItem("物攻手");
+                    var SPAIVEV = new ToolStripMenuItem("特攻手");
+                    var ATK_0SPEIVEV = new ToolStripMenuItem("物攻0速");
+                    var SPA_0SPEIVEV = new ToolStripMenuItem("特攻0速");
+                    var TANKIVEV = new ToolStripMenuItem("坦克");
+                    var IVEVN = new ToolStripMenuItem("快捷三维编辑器");
+                    var SavePDF = new ToolStripMenuItem("打印检测报告PDF");
+                    var clearnick = new ToolStripMenuItem("清除昵称垃圾字节");
+                    var changeid = new ToolStripMenuItem("把id由PKHeX改成存档id(仅适用于SV)");
+                    IVEVN.Image = Properties.Resources.Atom;
+                    SavePDF.Image = Properties.Resources.Report;
+                    clearnick.Image = Properties.Resources.TrashCan;
                     if (SaveFileEditor.SAV.Version is GameVersion.SL or GameVersion.VL)
                         changeid.Image = Properties.Resources.Transfer;
-                        menuVSD.Items.Insert(menuVSD.Items.Count, IVEVN);
-                        menuVSD.Items.Insert(menuVSD.Items.Count, SavePDF);
-                        menuVSD.Items.Insert(menuVSD.Items.Count, clearnick);
+                    menuVSD.Items.Insert(menuVSD.Items.Count, IVEVN);
+                    menuVSD.Items.Insert(menuVSD.Items.Count, SavePDF);
+                    menuVSD.Items.Insert(menuVSD.Items.Count, clearnick);
                     if (SaveFileEditor.SAV.Version is GameVersion.SL or GameVersion.VL)
                         menuVSD.Items.Insert(menuVSD.Items.Count, changeid);
-                        IVEVN.DropDownItems.Add(ATKIVEV);
-                        IVEVN.DropDownItems.Add(SPAIVEV);
-                        IVEVN.DropDownItems.Add(ATK_0SPEIVEV);
-                        IVEVN.DropDownItems.Add(SPA_0SPEIVEV);
-                        IVEVN.DropDownItems.Add(TANKIVEV);
-                        SavePDF.Click += (s, e) =>
-                        {
-                            pdf(la.Report(true), pk);
-                        };
-                        clearnick.Click += (s, e) =>
-                        {
-                            info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.Clearnike(pk));
-                        };
+                    IVEVN.DropDownItems.Add(ATKIVEV);
+                    IVEVN.DropDownItems.Add(SPAIVEV);
+                    IVEVN.DropDownItems.Add(ATK_0SPEIVEV);
+                    IVEVN.DropDownItems.Add(SPA_0SPEIVEV);
+                    IVEVN.DropDownItems.Add(TANKIVEV);
+                    SavePDF.Click += (s, e) =>
+                    {
+                        CreatePDF(la.Report(true), pk);
+                    };
+                    clearnick.Click += (s, e) =>
+                    {
+                        info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.Clearnike(pk));
+                    };
                     if(SaveFileEditor.SAV.Version is GameVersion.SL or GameVersion.VL)
-                        changeid.Click += (s, e) =>
-                        {
-                            info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.cid((PK9)pk, SaveFileEditor));
-                        };
-                        ATKIVEV.Click += (s, e) =>
-                        {
-                            info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.ATKIVEV(pk));
-                        };
-                        SPAIVEV.Click += (s, e) =>
-                        {
-                            info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.SPAIVEV(pk));
-                        };
-                        ATK_0SPEIVEV.Click += (s, e) =>
-                        {
-                            info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.ATK_0SPEIVEV(pk));
-                        };
-                        SPA_0SPEIVEV.Click += (s, e) =>
-                        {
-                            info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.SPA_0SPEIVEV(pk));
-                        };
-                        TANKIVEV.Click += (s, e) =>
-                        {
-                            info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.TANKIVEV(pk));
-                        };
-                        menuVSD.Closing += (s, e) => menuVSD.Items.Remove(IVEVN);
-                        menuVSD.Closing += (s, e) => menuVSD.Items.Remove(SavePDF);
-                        menuVSD.Closing += (s, e) => menuVSD.Items.Remove(clearnick);
-                        menuVSD.Closing += (s, e) => menuVSD.Items.Remove(changeid);
+                    changeid.Click += (s, e) =>
+                    {
+                        info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.cid((PK9)pk, SaveFileEditor));
+                    };
+                    ATKIVEV.Click += (s, e) =>
+                    {
+                        info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.ATKIVEV(pk));
+                    };
+                    SPAIVEV.Click += (s, e) =>
+                    {
+                        info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.SPAIVEV(pk));
+                    };
+                    ATK_0SPEIVEV.Click += (s, e) =>
+                    {
+                        info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.ATK_0SPEIVEV(pk));
+                    };
+                    SPA_0SPEIVEV.Click += (s, e) =>
+                    {
+                        info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.SPA_0SPEIVEV(pk));
+                    };
+                    TANKIVEV.Click += (s, e) =>
+                    {
+                        info.Slot.WriteTo(SaveFileEditor.SAV, CommonIVEVSetting.TANKIVEV(pk));
+                    };
+                    menuVSD.Closing += (s, e) => menuVSD.Items.Remove(insertSlotButton);
+                    menuVSD.Closing += (s, e) => menuVSD.Items.Remove(IVEVN);
+                    menuVSD.Closing += (s, e) => menuVSD.Items.Remove(SavePDF);
+                    menuVSD.Closing += (s, e) => menuVSD.Items.Remove(clearnick);
+                    menuVSD.Closing += (s, e) => menuVSD.Items.Remove(changeid);
                     }
-                
             };
         }
        
@@ -136,7 +135,63 @@ namespace WangPluginPkm.Plugins
             sender = pb;
             return new SlotViewInfo<PictureBox>(loc, view);
         }
-        private static async void pdf(string result, PKM p)
+        public static T? GetUnderlyingControl<T>(object sender) where T : class
+        {
+            while (true)
+            {
+                switch (sender)
+                {
+                    case T p:
+                        return p;
+                    case ToolStripItem { Owner: { } o }:
+                        sender = o;
+                        continue;
+                    case ContextMenuStrip { SourceControl: { } s }:
+                        sender = s;
+                        continue;
+                    default:
+                        return default;
+                }
+            }
+        }
+        public static T? FindFirstControlOfType<T>(Control aParent) where T : class
+        {
+            while (true)
+            {
+                if (aParent is T t)
+                    return t;
+
+                if (aParent.Parent != null)
+                    aParent = aParent.Parent;
+                else
+                    return null;
+            }
+        }
+        private void InsertSlot(int boxNum, int slotNum)
+        {
+            int startIndex = boxNum * SaveFileEditor.SAV.BoxSlotCount + slotNum;
+            int boxIndex = startIndex + 1;
+            PKM currMon, nextMon;
+            while (boxIndex < SaveFileEditor.SAV.SlotCount)
+            {
+                currMon = SaveFileEditor.SAV.GetBoxSlotAtIndex(boxIndex);
+                if (currMon.Species == (int)Species.None) break;
+                boxIndex++;
+            }
+            currMon = SaveFileEditor.SAV.GetBoxSlotAtIndex(startIndex);
+            SaveFileEditor.SAV.SetBoxSlotAtIndex(SaveFileEditor.SAV.BlankPKM, startIndex);
+            for (int index = startIndex + 1; index <= boxIndex; index++)
+            {
+                StorageSlotSource slotSource = SaveFileEditor.SAV.GetSlotFlags(index);
+                if (slotSource.IsOverwriteProtected()) continue;
+                nextMon = SaveFileEditor.SAV.GetBoxSlotAtIndex(index);
+                SaveFileEditor.SAV.SetBoxSlotAtIndex(currMon, index, PKMImportSetting.UseDefault, PKMImportSetting.Skip);
+                currMon = nextMon;
+            }
+            SaveFileEditor.ReloadSlots();
+        }
+
+        private static async void CreatePDF(string result, PKM p)
         {
             string sp = p.Species.ToString().PadLeft(4, '0');
             string baseuri = "https://raw.githubusercontent.com/8CBCA403/pokepic/main/Normal/poke_capture_"+$"{sp}"+"_000_00000000_f_n.png";
@@ -181,6 +236,7 @@ namespace WangPluginPkm.Plugins
         }
         public async static Task<Bitmap> LoadImage(Uri uri)
         {
+            Bitmap bitmapImage=new(Properties.Resources.SuperWang);
             try
             {
                 using (HttpClient client = new HttpClient())
@@ -189,83 +245,21 @@ namespace WangPluginPkm.Plugins
                     {
                         response.EnsureSuccessStatusCode();
                         System.IO.Stream responseStream = response.Content.ReadAsStream();
-                        Bitmap bitmapImage = new Bitmap(responseStream);
+                        bitmapImage = new Bitmap(responseStream);
                         return bitmapImage;
                     }
                 }
             }
             catch (Exception ex)
             {
-                Debug.WriteLine("Failed to load the image: {0}", ex.Message);
+                Debug.WriteLine("下载图片失败: {0}", ex.Message);
             }
-            return null;
+            return bitmapImage;
         }
-        public static T? GetUnderlyingControl<T>(object sender) where T : class
-        {
-            while (true)
-            {
-                switch (sender)
-                {
-                    case T p:
-                        return p;
-                    case ToolStripItem { Owner: { } o }:
-                        sender = o;
-                        continue;
-                    case ContextMenuStrip { SourceControl: { } s }:
-                        sender = s;
-                        continue;
-                    default:
-                        return default;
-                }
-            }
-        }
-        public static T? FindFirstControlOfType<T>(Control aParent) where T : class
-        {
-            while (true)
-            {
-                if (aParent is T t)
-                    return t;
-
-                if (aParent.Parent != null)
-                    aParent = aParent.Parent;
-                else
-                    return null;
-            }
-        }
+      
 #nullable disable
 
-        private void OpenForm(object sender, EventArgs e)
-        {
-
-            var form = new SimpleEditor(SaveFileEditor, PKMEditor);
-            form.Show();
-        }
-        private void InsertSlot(int boxNum, int slotNum)
-        {
-            int startIndex = boxNum * SaveFileEditor.SAV.BoxSlotCount + slotNum;
-            int boxIndex = startIndex + 1;
-            PKM currMon, nextMon;
-            while (boxIndex < SaveFileEditor.SAV.SlotCount)
-            {
-                currMon = SaveFileEditor.SAV.GetBoxSlotAtIndex(boxIndex);
-                if (currMon.Species == (int)Species.None) break;
-                boxIndex++;
-            }
-            if (boxIndex == SaveFileEditor.SAV.SlotCount)
-            {
-              //  MessageBox.Show(string.Format(Language.NoEmptySlotsError, boxNum + 1, slotNum + 1), Language.FormTitle, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-            currMon = SaveFileEditor.SAV.GetBoxSlotAtIndex(startIndex);
-            SaveFileEditor.SAV.SetBoxSlotAtIndex(SaveFileEditor.SAV.BlankPKM, startIndex);
-            for (int index = startIndex + 1; index <= boxIndex; index++)
-            {
-                nextMon = SaveFileEditor.SAV.GetBoxSlotAtIndex(index);
-                SaveFileEditor.SAV.SetBoxSlotAtIndex(currMon, index, PKMImportSetting.UseDefault, PKMImportSetting.Skip);
-                currMon = nextMon;
-            }
-            SaveFileEditor.ReloadSlots();
-        }
-
+     
+        
     }
 }
