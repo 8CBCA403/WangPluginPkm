2ccdf0b186cb4d8988a615610f4d7670a9c5b9f5
8CBCA403 <c18519667281@gmail.com>
2025-07-12 18:02:49 +0800

    Set main window icon using resource on startup
    Added support for setting the main form's icon from embedded resources by introducing a timer to update the icon after startup. Updated resource files and code references to use the new icon, and added Icon.bmp and updated icon.ico assets.

--- WangPluginPkm/Properties/Resources.Designer.cs
+++ WangPluginPkm/Properties/Resources.Designer.cs
@@ -211,6 +211,16 @@ namespace WangPluginPkm.Properties {
         }
         
         /// <summary>
+        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Icon icon {
+            get {
+                object obj = ResourceManager.GetObject("icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
         internal static System.Drawing.Bitmap ID {
--- WangPluginPkm/WangPluginPkm.cs
+++ WangPluginPkm/WangPluginPkm.cs
@@ -1,8 +1,10 @@
 ﻿using PKHeX.Core;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Media;
 using System.Windows.Forms;
+using WangPluginPkm.PluginUtil;
 
 namespace WangPluginPkm
 {
@@ -24,6 +26,23 @@ namespace WangPluginPkm
         {
             var config = PluginConfig.LoadConfig();
             globalArgs = args;
+            var timer = new System.Windows.Forms.Timer();
+            timer.Interval = 500; // 延迟 0.5 秒
+            timer.Tick += (_, _) =>
+            {
+                timer.Stop(); // 只执行一次
+
+                // 查找主窗体
+                var mainForm = Application.OpenForms
+                    .Cast<Form>()
+                    .FirstOrDefault(f => f.GetType().Name.Contains("PKHeX") || f.Name.Contains("Main"));
+
+                if (mainForm != null)
+                {
+                    mainForm.Icon = Properties.Resources.icon;
+                }
+            };
+            timer.Start();
             SaveFileEditor = (ISaveFileProvider)Array.Find(args, z => z is ISaveFileProvider);
             PKMEditor = (IPKMView)Array.Find(args, z => z is IPKMView);
             var menu = (ToolStrip)Array.Find(args, z => z is ToolStrip);
@@ -42,9 +61,9 @@ namespace WangPluginPkm
             var toolsitems = tools.DropDownItems;
             var modmenusearch = toolsitems.Find(ParentMenuName, false);
             var modmenu = GetModMenu(tools, modmenusearch);
-            var form = tools.GetCurrentParent().FindForm();
-            if (form is not null)
-                form.Icon = Properties.Resources.SW;
+            //var form = tools.GetCurrentParent().FindForm();
+          //  if (form is not null)
+             //   form.Icon = Properties.Resources.icon;
             AddPluginControl(modmenu);
         }
         private static ToolStripMenuItem GetModMenu(ToolStripDropDownItem tools, IReadOnlyList<ToolStripItem> search)
